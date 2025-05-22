aa25ff4ef5b42c2a35505447a0845095af6276f4
thkim6010 <thkim6010@gmail.com>
2025-05-22 12:24:17 +0900

    Rename folders and files to English names

--- accidentPro.py
+++ accidentPro.py
@@ -35,9 +35,9 @@ st.title("운동 데이터 분류 및 위험 상태 탐지")
 # 1. 학습용 데이터 로드 (로컬 csv파일 경로 수정 필요)
 @st.cache_data
 def load_training_data():
-    walking_file = "walk\\walk.csv"
-    running_file = "run\\run.csv"
-    stairs_file = "stair\\stair.csv"
+    walking_file = "walk/walk.csv"
+    running_file = "run/run.csv"
+    stairs_file = "stair/stair.csv"
 
     df_walking = pd.read_csv(walking_file)
     df_running = pd.read_csv(running_file)
